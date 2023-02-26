40cb4a8f27d30acb65b063fdb2e17661a415fc01
Derrick <dgkrieger@gmail.com>
2023-02-26 17:01:00 -0600

    Updated. Budget has working login

---
+++ budget/src/test/java/com/frontrow/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontrow.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
