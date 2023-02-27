99a7ea8bf4e0f6d1501b58d39498d9967fb5caa3
Derrick <krieger@ucmo.edu>
2023-02-27 11:41:34 -0600

    New database create script

--- Resources/CommerceBank.sql
+++ Resources/CommerceBank.sql
@@ -1,77 +1,30 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: commercebank
--- ------------------------------------------------------
--- Server version	8.0.32
+/* Save and run the entire script in one file */
+/* Run this from the root */
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+drop database if exists commerce;
+create database commerce;
 
---
--- Table structure for table `calendar`
---
+use commerce;
 
-DROP TABLE IF EXISTS `calendar`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `calendar` (
-  `username` varchar(50) DEFAULT NULL,
-  `type` varchar(1) DEFAULT NULL,
-  `amount` decimal(10,2) DEFAULT NULL,
-  `time` date DEFAULT NULL,
-  KEY `username` (`username`),
-  CONSTRAINT `calendar_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+drop table if exists users;
+drop table if exists calendar;
 
---
--- Dumping data for table `calendar`
---
+create table users(
+  u_id int not null AUTO_INCREMENT,
+  username varchar(50), 
+  password varchar(50), 
+  first_name varchar(30), 
+  constraint pkusers primary key (u_id));
 
-LOCK TABLES `calendar` WRITE;
-/*!40000 ALTER TABLE `calendar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calendar` ENABLE KEYS */;
-UNLOCK TABLES;
+ALTER TABLE users AUTO_INCREMENT=100;
 
---
--- Table structure for table `users`
---
+create table calendar(
+    c_id int(10) not null,
+    u_id int(10) not null,
+    amount decimal(10,2) DEFAULT NULL,
+    transactionTime date,
+    constraint pkcalendar primary key (c_id));
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE calendar ADD CONSTRAINT calendar_FK FOREIGN KEY (u_id) REFERENCES users(u_id);
 
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('Bobsmith','1234'),('Mary','admin');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-02-24 15:26:53
+INSERT INTO `users` (username,password,first_name) VALUES ('Bobsmith','1234','Bob'),('Mary','admin','Mary'),('customer1','1234','Chris');
